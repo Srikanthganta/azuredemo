f048084a26d8211d896fdd243425f7e704df62dc
Sganta <srikanth_ganta@gap.com>
2022-05-14 16:20:37 +0530

    First commit

---
+++ src/main/java/com/spring/sample/SpringAzureSampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureSampleApplication.class, args);
+	}
+
+}
